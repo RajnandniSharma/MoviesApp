6520f445267bb8522bcab2f2fa0644307afcad45
RajnandniSharma <rajnandnis61@gmail.com>
2022-10-29 17:36:23 +0530

    first commit

---
+++ MoviesApp/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApp.Models;
+
+namespace MoviesApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+      
+            public IActionResult Index()
+        {
+            IMemory Im=new IMemory();
+            return View(Im.getAllMovies());
+        }
+    }
+}
---
+++ MoviesApp/Models/IMemory.cs
@@ -0,0 +1,17 @@
+﻿namespace MoviesApp.Models
+{
+    public class IMemory
+    {
+        List<Movies> Movies = new List<Movies>()
+        {
+            new Movies(){Id=1,Title="DDLJ",Description="The best Movies of SRK",Rating = 10},
+            new Movies(){Id=2,Title="PK",Description="The best Movies of Aamir",Rating = 10},
+            new Movies(){Id=3,Title="Ready",Description="The best Movies of Salman",Rating = 10},
+        };
+
+        public List<Movies> getAllMovies()
+        {
+            return Movies;
+        }
+    }
+}
